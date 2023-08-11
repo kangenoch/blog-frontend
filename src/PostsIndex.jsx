c792ca098942a702b64b8dc3b1b9ffe0d5cefe9a
enoch <enokkang@gmail.com>
2023-08-11 14:59:57 -0500

    adds bootstrap cards

--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -2,16 +2,27 @@
 export function PostsIndex(props) {
   console.log(props);
   return (
-    <div id="posts-index">
-      <h1>All posts</h1>
-      {props.posts.map((post) => (
-        <div key={post.id} className="posts">
-          <h2>{post.title}</h2>
-          <img src={post.image} alt={post.title} />
-          <p>Body: {post.body}</p>
-          <button onClick={() => props.onShowPost(post)}>More info</button>
-        </div>
-      ))}
-    </div>
+    <>
+      <div id="posts-index">
+        <h1>All posts</h1>
+        {props.posts.map((post) => (
+          <div key={post.id} className="posts">
+            <>
+              <div className="card">
+                <img src={post.image} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">{post.title}</h5>
+                  <p className="card-text">Body: {post.body}</p>
+                  <a href={() => props.onShowPost(post)} className="btn btn-primary">
+                    More Info
+                  </a>
+                  <button onClick={() => props.onShowPost(post)}>More info</button>
+                </div>
+              </div>
+            </>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
