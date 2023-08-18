07c6df13349c53236f243273735b18e994f331e1
enoch <enokkang@gmail.com>
2023-08-18 14:24:48 -0500

    adds ReactPlayer library

--- src/About.jsx
+++ src/About.jsx
@@ -1,5 +1,6 @@
 // import React from "react";
 import { Map, Marker } from "pigeon-maps";
+import ReactPlayer from "react-player";
 
 export function About() {
   return (
@@ -10,6 +11,9 @@ export function About() {
         <Map height={300} defaultCenter={[42.1, -87.3]} defaultZoom={8.5}>
           <Marker width={50} anchor={[42.2977, -87.8872]} />
         </Map>
+      </p>
+      <p>
+        <ReactPlayer url="https://streamable.com/moo" loop={true} width="300" />
       </p>
     </div>
   );
